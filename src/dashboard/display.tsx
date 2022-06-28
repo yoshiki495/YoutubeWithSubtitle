2117b3920114e01f6ca9c3f7aeb7eb8d4439ed87
yoshiki <tanayoshi78907890@icloud.com>
2022-06-28 17:16:50 +0900

    set youtube movie

--- src/dashboard/display.tsx
+++ src/dashboard/display.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import YouTubeContent from './youtube'
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Dvr from '@mui/icons-material/Dvr';
@@ -55,35 +56,37 @@ export default function Album() {
             pb: 6,
           }}
         >
-            <Grid item xs={6}>
-                
-            </Grid>
-            <Grid item xs={6}>
-                <Container maxWidth="sm">
-                    <Typography
-                    component="h1"
-                    variant="h2"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                    >
-                    Album layout
-                    </Typography>
-                    <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                    Something short and leading about the collection below—its contents,
-                    the creator, etc. Make it short and sweet, but not too short so folks
-                    don&apos;t simply skip over it entirely.
-                    </Typography>
-                    <Stack
-                    sx={{ pt: 4 }}
-                    direction="row"
-                    spacing={2}
-                    justifyContent="center"
-                    >
-                    <Button variant="contained">Main call to action</Button>
-                    <Button variant="outlined">Secondary action</Button>
-                    </Stack>
-                </Container>
+            <Grid container>
+                <Grid item md={6} sx={{ pl: 8 }}>
+                    <YouTubeContent />
+                </Grid>
+                <Grid item md={6} sx={{ pr: 8 }}>
+                    <Container maxWidth="sm">
+                        <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                        >
+                        Album layout
+                        </Typography>
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        Something short and leading about the collection below—its contents,
+                        the creator, etc. Make it short and sweet, but not too short so folks
+                        don&apos;t simply skip over it entirely.
+                        </Typography>
+                        <Stack
+                        sx={{ pt: 4 }}
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center"
+                        >
+                        <Button variant="contained">Main call to action</Button>
+                        <Button variant="outlined">Secondary action</Button>
+                        </Stack>
+                    </Container>
+                </Grid>
             </Grid>
         </Box>
       </main>
---
+++ src/dashboard/youtube.tsx
@@ -0,0 +1,9 @@
+import YouTube from 'react-youtube';
+
+function YouTubeContent() {
+  return (
+    <YouTube videoId="動画ID" />
+  );
+}
+
+export default YouTubeContent;
