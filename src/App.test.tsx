c182101843e05c5ea1422721bdee167ca1d002ba
yoshiki <tanayoshi78907890@icloud.com>
2022-06-30 14:23:30 +0900

    fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Album from './dashboard/display';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Album />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
